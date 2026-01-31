bc8f8528b76b823c1e5229a997ca20dca3a376b7
Валерий Иванов <valeraivanov2681@gmail.com>
2026-01-31 14:33:28 +0300

    initial commit

---
+++ inc/common.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "inc/common.h" 
+
+int main(void)
+{
+    while(1)
+    {
+
+    }
+}
